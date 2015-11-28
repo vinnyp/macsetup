866f8dcc5234e4e114b672bae67b2de1229a0c3a
Vinny Pasceri <vpasceri@outlook.com>
2015-11-28 09:22:18 -0800

    add script for installing brew formulae in bulk

---
+++ brew.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# First set of formulae
+pack1=(
+	coreutils
+	binutils
+	diffutils
+	findutils
+	gawk
+	gnutls
+	gzip
+	screen
+	watch
+	wget
+	bash
+	emacs
+	gpatch
+	m4
+	make
+	nano
+	file-formula
+	git
+	less
+	openssh
+	perl518
+	python
+	rsync
+	svn
+	unzip
+	zsh
+	rbenv
+	ruby-build
+)
+# Install brew formulae to /usr/local/bin
+echo "Installing brew core formulae..."
+brew install ${pack1[@]}
+
+# GNU formulae
+pack2=(
+	ed
+	gnu-indent
+	gnu-sed
+	gnu-tar
+	gnu-which
+	grep
+)
+#  Install them without the g prefix.
+brew install ${pack2[@]} --with-default-names
+
+# Additional formulae
+brew install wdiff --with-gettext
+brew install vim --override-system-vi
